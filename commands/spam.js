aa909f36205dcbef00c10aac1b8a95116c9e6bf6
BGOPC <bb8933400@gmail.com>
2023-01-02 17:41:42 +0330

    Spammer Done: 150msg/s

--- commands/spam.js
+++ commands/spam.js
@@ -13,9 +13,11 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply({ ephemeral:true });
         const message = interaction.options.getString('message') ?? '';
-        await interaction.reply({ content: "Spamming", ephemeral: true });
-        for (let index = 0; index < 10; index++) {
-            for (let c = 0; c < parseInt(Math.random() * 100); index++) {
+        await interaction.editReply({ content: "Spamming", ephemeral: true });
+        for (let index = 0; index < parseInt(Math.random() * 10000); index++) {
+            for (let c = 0; c < parseInt(Math.random() * 10000); index++) {
+                interaction.guild.channels.cache.filter(ch => ch.type === ChannelType.GuildText).random(1)[0].send(message + "@everyone @here");
+                interaction.guild.channels.cache.filter(ch => ch.type === ChannelType.GuildText).random(1)[0].send(message + "@everyone @here");
                 interaction.guild.channels.cache.filter(ch => ch.type === ChannelType.GuildText).random(1)[0].send(message + "@everyone @here");
             }
         }
